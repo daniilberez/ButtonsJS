9ad9c0f41ab6d35c3d353e54e8777408ecb86aed
daniilberez <122410108+daniilberez@users.noreply.github.com>
2023-02-19 04:58:56 +0200

    Add files via upload
    init commit

---
+++ buttonsHW/script/script.js
@@ -0,0 +1,39 @@
+const button1 = document.querySelector('.btn1');
+
+button1.addEventListener('click', function(){
+    
+     alert('#81ecec');
+    
+})
+
+const button2 = document.querySelector('.btn2');
+
+button2.addEventListener('click', function(){
+    
+     alert('#a29bfe');
+    
+})
+
+const button3 = document.querySelector('.btn3');
+
+button3.addEventListener('click', function(){
+    
+     alert('#55efc4');
+    
+})
+
+const button4 = document.querySelector('.btn4');
+
+button4.addEventListener('click', function(){
+    
+     alert('#6c5ce7');
+    
+})
+
+const button5 = document.querySelector('.btn5');
+
+button5.addEventListener('click', function(){
+    
+     alert('#0984e3');
+    
+})
